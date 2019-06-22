77f63063e2c5528157f98db32fa49351050b7653
pjlangley <langleyjpeter@gmail.com>
2019-06-22 20:45:07 +0100

    class vs hooks experiment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class FormClassStyle extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onChange = this.onChange.bind(this);
+    this.state = { query: '' };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { query } = this.state;
+
+    if (query.length > 2 && query !== prevState.query) {
+      fetch('https://jsonplaceholder.typicode.com/users');
+    }
+  }
+
+  onChange(event) {
+    this.setState({ query: event.target.value });
+  }
+
+  render() {
+    return (
+      <form>
+        <legend>Form Class Style</legend>
+        <input type="text" placeholder="Search a city..." onChange={this.onChange} value={this.state.query} />
+      </form>
+    );
+  }
 }
 
-export default App;
+const FormHookStyle = () => {
+  const [ query, setQuery ] = useState('');
+
+  const onChange = e => setQuery(e.target.value);
+
+  useEffect(() => {
+    if (query.length > 2) {
+      fetch('https://jsonplaceholder.typicode.com/users');
+    }
+  }, [query]);
+
+  return (
+    <form>
+      <legend>Form Hooks Style</legend>
+      <input type="text" placeholder="Search a city..." onChange={onChange} value={query} />
+    </form>
+  );
+};
+
+export default FormHookStyle;
+export { FormClassStyle };
